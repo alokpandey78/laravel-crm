6d3a9463317730cc13263b427e8e1a2940bea568
Alok Pandey <alokpandey78@gmail.com>
2025-08-28 00:47:17 +0530

    Added Customer model, migration & controller
    Added Customer model, migration & controller

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\CustomerController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('customers', CustomerController::class)->middleware(['auth']);
